5f35a25646f301828a0189e491aae1305b1918d4
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2023-10-12 15:11:16 +0000

    explain

--- 1.Lecture-C/lessons/hello2.c
+++ 1.Lecture-C/lessons/hello2.c
@@ -1,10 +1,28 @@
 // get_string and printf with %s
 
+// Description: This C program collects the user's name as a string and prints a greeting using the correct placeholder "%s" for the name.
+
+// Include necessary libraries: cs50.h for get_string and stdio.h for printf.
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
+    // Get the user's name as a string and store it in the "answer" variable.
     string answer = get_string("What's your name? ");
+
+    // Use printf with the correct placeholder "%s" to display a greeting along with the user's name.
     printf("hello, %s\n", answer);
 }
+
+// Explanation:
+
+// The comments at the top provide a brief description of the program's purpose, which is to collect the user's name as a string and correctly print a greeting using the placeholder "%s" for the name.
+
+// The #include <cs50.h> and #include <stdio.h> lines include the necessary libraries for using the get_string and printf functions.
+
+// Inside the main function, the string answer = get_string("What's your name? "); line collects the user's name as a string and stores it in the "answer" variable.
+
+// The printf("hello, %s\n", answer); line is used to print a greeting, and it correctly uses the placeholder "%s" to insert the user's name in the output.
+
+// This code demonstrates the proper usage of the printf function in C to display text along with variable values.
