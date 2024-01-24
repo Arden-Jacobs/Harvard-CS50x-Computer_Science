742b365558ca2d9c9472b162ab8d6e2e6f5d3d15
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2024-01-24 17:14:42 +0200

    explain

--- 2.Lecture-Array/lessons/hi2.c
+++ 2.Lecture-Array/lessons/hi2.c
@@ -1,10 +1,13 @@
-// Prints string
-
+// Include necessary libraries for the program
 #include <cs50.h>
 #include <stdio.h>
 
+// Main function, entry point of the program
 int main(void)
 {
+    // Declare and initialize a string variable
     string s = "HI!";
+
+    // Print the string
     printf("%s\n", s);
 }
