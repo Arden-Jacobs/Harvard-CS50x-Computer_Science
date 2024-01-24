3ad23c5b4d365f7c7dfc382320146ad66696c4ef
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2024-01-24 17:17:23 +0200

    explain

--- 2.Lecture-Array/lessons/hi5.c
+++ 2.Lecture-Array/lessons/hi5.c
@@ -1,13 +1,17 @@
-// Multiple strings
-
+// Include necessary libraries for the program
 #include <cs50.h>
 #include <stdio.h>
 
+// Main function, entry point of the program
 int main(void)
 {
+    // Declare and initialize two string variables
     string s = "HI!";
     string t = "BYE!";
 
+    // Print the first string
     printf("%s\n", s);
+
+    // Print the second string
     printf("%s\n", t);
 }
