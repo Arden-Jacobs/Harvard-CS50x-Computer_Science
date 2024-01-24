fe5e283547fd0a94c06da90495b04f69ae8b0a01
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2024-01-24 17:21:47 +0200

    explain

--- 2.Lecture-Array/lessons/length1.c
+++ 2.Lecture-Array/lessons/length1.c
@@ -1,25 +1,33 @@
-// Determines the length of a string using a function
-
+// Include necessary libraries for the program
 #include <cs50.h>
 #include <stdio.h>
 
+// Function prototype for string_length
 int string_length(string s);
 
+// Main function, entry point of the program
 int main(void)
 {
-    // Prompt for user's name
+    // Prompt the user for input, specifically a string representing their name
     string name = get_string("Name: ");
+    
+    // Call the function string_length and store the result in the variable 'length'
     int length = string_length(name);
+
+    // Print the length of the entered string
     printf("%i\n", length);
 }
 
+// Function to determine the length of a string
 int string_length(string s)
 {
-    // Count number of characters up until '\0' (aka NUL)
+    // Count the number of characters in the string until the null terminator '\0' is encountered
     int n = 0;
     while (s[n] != '\0')
     {
         n++;
     }
+
+    // Return the length of the string
     return n;
 }
