f930b2a92f5f324612b4c02fca4506e1fc772d2a
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2024-01-24 17:18:16 +0200

    explain

--- 2.Lecture-Array/lessons/hi6.c
+++ 2.Lecture-Array/lessons/hi6.c
@@ -1,15 +1,20 @@
-// Array of strings
-
+// Include necessary libraries for the program
 #include <cs50.h>
 #include <stdio.h>
 
+// Main function, entry point of the program
 int main(void)
 {
+    // Declare an array of strings and initialize its size
     string words[2];
 
+    // Assign values to individual elements of the array
     words[0] = "HI!";
     words[1] = "BYE!";
 
+    // Print the first string from the array
     printf("%s\n", words[0]);
+
+    // Print the second string from the array
     printf("%s\n", words[1]);
 }
