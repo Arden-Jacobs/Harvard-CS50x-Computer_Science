16cfa01d726a3b72edf3e20b6993333db0cd4a80
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2024-01-24 17:15:30 +0200

    explain

--- 2.Lecture-Array/lessons/hi3.c
+++ 2.Lecture-Array/lessons/hi3.c
@@ -1,10 +1,13 @@
-// Prints string's ASCII codes
-
+// Include necessary libraries for the program
 #include <cs50.h>
 #include <stdio.h>
 
+// Main function, entry point of the program
 int main(void)
 {
+    // Declare and initialize a string variable
     string s = "HI!";
+
+    // Print the ASCII codes of the characters in the string
     printf("%i %i %i\n", s[0], s[1], s[2]);
 }
