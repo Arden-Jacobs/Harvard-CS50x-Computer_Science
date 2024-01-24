5805024415e1bfbce7d30a8fd42bf5fb315e0709
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2024-01-24 17:12:12 +0200

    explain

--- 2.Lecture-Array/lessons/hello1.c
+++ 2.Lecture-Array/lessons/hello1.c
@@ -1,8 +1,13 @@
+// Include necessary libraries for the program
 #include <cs50.h>
 #include <stdio.h>
 
+// Main function, entry point of the program
 int main(void)
 {
+    // Prompt the user for input, specifically a string representing their name
     string name = get_string("What's your name? ");
+    
+    // Print a greeting message using the provided name
     printf("hello, %s\n", name);
 }
