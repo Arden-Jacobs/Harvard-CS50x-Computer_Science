c23ec8d3c023ef1b3ce6c09b4ccf8af7fce72fda
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2024-01-24 17:07:03 +0200

    explain

--- 2.Lecture-Array/lessons/greet0.c
+++ 2.Lecture-Array/lessons/greet0.c
@@ -1,10 +1,13 @@
-// Uses get_string
-
+// Include necessary libraries for the program
 #include <cs50.h>
 #include <stdio.h>
 
+// Main function, entry point of the program
 int main(void)
 {
+    // Prompt the user for input, specifically a string representing their name
     string answer = get_string("What's your name? ");
+    
+    // Print a greeting message using the provided name
     printf("hello, %s\n", answer);
 }
