25c7fc4deebbf5bd40939679698d00f72a9af00d
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2024-01-24 17:11:15 +0200

    explain

--- 2.Lecture-Array/lessons/hello0.c
+++ 2.Lecture-Array/lessons/hello0.c
@@ -1,6 +1,9 @@
+// Include necessary library for the program
 #include <stdio.h>
 
+// Main function, entry point of the program
 int main(void)
 {
+    // Print a greeting message to the console
     printf("hello, world\n");
 }
