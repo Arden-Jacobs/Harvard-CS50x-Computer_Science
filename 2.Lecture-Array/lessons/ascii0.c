e013913551429a48490038b42e8a1427273b347c
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2024-01-24 16:49:00 +0200

    explain

--- 2.Lecture-Array/lessons/ascii0.c
+++ 2.Lecture-Array/lessons/ascii0.c
@@ -1,15 +1,21 @@
-// Explicitly casts chars to ints
-
+// Include necessary libraries for the program
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
 
+// Main function, entry point of the program
 int main(void)
 {
+    // Prompt the user for input, store it in the variable 's'
     string s = get_string("String: ");
+
+    // Iterate over each character in the string 's'
     for (int i = 0; i < strlen(s); i++)
     {
+        // Explicitly cast the current character 's[i]' to an integer and store it in 'c'
         int c = (int) s[i];
+
+        // Print the current character and its corresponding ASCII value
         printf("%c %i\n", s[i], c);
     }
 }
