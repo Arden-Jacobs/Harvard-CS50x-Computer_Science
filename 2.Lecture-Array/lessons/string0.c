a47beaac7b1663f18f25f3acef2d6104b9b9e16a
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2024-01-24 17:30:26 +0200

    explain

--- 2.Lecture-Array/lessons/string0.c
+++ 2.Lecture-Array/lessons/string0.c
@@ -1,12 +1,15 @@
-// Prints string char by char, using strlen
-
+// Include necessary libraries for the program
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
 
+// Main function, entry point of the program
 int main(void)
 {
+    // Prompt the user for input, specifically a string
     string s = get_string("Input:  ");
+
+    // Print each character of the string one by one using strlen
     printf("Output: ");
     for (int i = 0; i < strlen(s); i++)
     {
