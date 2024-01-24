8e8c91fbe9aec603ac9303487198cb603ed62dbf
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2024-01-24 17:14:02 +0200

    explain

--- 2.Lecture-Array/lessons/hi1.c
+++ 2.Lecture-Array/lessons/hi1.c
@@ -1,12 +1,14 @@
-// Prints chars' ASCII codes
-
+// Include necessary library for the program
 #include <stdio.h>
 
+// Main function, entry point of the program
 int main(void)
 {
+    // Declare and initialize three character variables
     char c1 = 'H';
     char c2 = 'I';
     char c3 = '!';
 
+    // Print the ASCII codes of the characters in sequence
     printf("%i %i %i\n", c1, c2, c3);
 }
