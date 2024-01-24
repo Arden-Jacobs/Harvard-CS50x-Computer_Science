c62660c057de329a820225ba4c61574bf68b628b
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2024-01-24 17:16:09 +0200

    explain

--- 2.Lecture-Array/lessons/hi4.c
+++ 2.Lecture-Array/lessons/hi4.c
@@ -1,10 +1,13 @@
-// Prints string's ASCII codes, including NUL
-
+// Include necessary libraries for the program
 #include <cs50.h>
 #include <stdio.h>
 
+// Main function, entry point of the program
 int main(void)
 {
+    // Declare and initialize a string variable
     string s = "HI!";
-    printf("%i %i %i %i\n", s[0], s[1], s[2], s[3]);
+
+    // Print the ASCII codes of the characters in the string
+    printf("%i %i %i\n", s[0], s[1], s[2]);
 }
