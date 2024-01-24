c974eecbcb4dc2a06defc5db24a002190ceaf5a4
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2024-01-24 17:10:20 +0200

    explain

--- 2.Lecture-Array/lessons/greet3.c
+++ 2.Lecture-Array/lessons/greet3.c
@@ -1,12 +1,14 @@
-// Prints command-line arguments
-
+// Include necessary libraries for the program
 #include <cs50.h>
 #include <stdio.h>
 
+// Main function, entry point of the program, accepts command-line arguments
 int main(int argc, string argv[])
 {
+    // Iterate over each command-line argument
     for (int i = 0; i < argc; i++)
     {
+        // Print the current command-line argument
         printf("%s\n", argv[i]);
     }
 }
