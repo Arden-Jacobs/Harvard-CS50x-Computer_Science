ade761fe4ed8be8ffc9878414d9f476c02451ffc
Arden's Professional Account <142479499+Arden-Jacobs@users.noreply.github.com>
2024-01-24 17:24:44 +0200

    explain

--- 2.Lecture-Array/lessons/scores1.c
+++ 2.Lecture-Array/lessons/scores1.c
@@ -1,16 +1,18 @@
-// Averages three (hardcoded) numbers using an array
-
+// Include necessary libraries for the program
 #include <cs50.h>
 #include <stdio.h>
 
+// Main function, entry point of the program
 int main(void)
 {
-    // Scores
+    // Declare an array of integers to store scores
     int scores[3];
+
+    // Assign values to individual elements of the array
     scores[0] = 72;
     scores[1] = 73;
     scores[2] = 33;
 
-    // Print average
+    // Print the average of the scores using an array
     printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
