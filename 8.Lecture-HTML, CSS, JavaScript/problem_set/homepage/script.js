7436110cd2b3b3440c944dbb21ff6d7ef712e6a0
Arden-Jacobs <142479499+Arden-Jacobs@users.noreply.github.com>
2024-09-10 11:19:26 +0000

    homepage

---
+++ 8.Lecture-HTML, CSS, JavaScript/problem_set/homepage/script.js
@@ -0,0 +1,8 @@
+/*============== typed js ================*/
+const typed = new Typed(".multiple-text", {
+    strings: ["Software Engineer", "Entrepreneur", "Computer Scientist"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
